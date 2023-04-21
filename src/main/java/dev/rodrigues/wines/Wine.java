2de3fe82ab27186810127cc531b62a2d09a1d7ce
JessicaRodriguesFelix <jessicarodrigues_felix@hotmail.com>
2023-04-21 09:38:33 -0400

    create wine and review classes

---
+++ src/main/java/dev/rodrigues/wines/Wine.java
@@ -0,0 +1,27 @@
+package dev.rodrigues.wines;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document(collection = "wines")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Wine {
+    @Id
+    private ObjectId id;
+    private String winery;
+    private String wine;
+    private String location;
+    private String image;
+    private String imdbId;
+    @DocumentReference
+    private List<Review> reviewIds;
+}
--- src/main/java/dev/rodrigues/wines/WinesApplication.java
+++ src/main/java/dev/rodrigues/wines/WinesApplication.java
@@ -6,15 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class WinesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WinesApplication.class, args);
 	}
 
-	@GetMapping("/")
-	public String apiRoot(){
-		return "Hello Wines";
-	}
+
 }
