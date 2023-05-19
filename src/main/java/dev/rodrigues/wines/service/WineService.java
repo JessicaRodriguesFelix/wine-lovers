278d9a9a41b5bca15d122529edb088a13ab2cedc
JessicaRodriguesFelix <jessicarodrigues_felix@hotmail.com>
2023-05-19 10:26:27 -0400

    remove whitespace in Wine service

--- src/main/java/dev/rodrigues/wines/service/WineService.java
+++ src/main/java/dev/rodrigues/wines/service/WineService.java
@@ -5,7 +5,6 @@ import dev.rodrigues.wines.repository.WineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 
